2aa9384cf7ac97117a336cf35447772b7909b56f
Miguel Peralta <maikperalta123@gmail.com>
2023-04-06 14:06:34 -0700

    Ejercicio terminado

---
+++ ejercicio_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escribe tu nombre: ");
+            string nombre = Console.ReadLine();
+            Console.Write("Escribe tu email: ");
+            string email = Console.ReadLine();
+            Console.Write("¿Cuál es el precio del articulo que desea comprar? ");
+            double precio = Convert.ToDouble(Console.ReadLine());
+            Console.Write("¿Cuenta con algún cúpón de descuento? (s/n): ");
+            char respuesta = Convert.ToChar(Console.ReadLine());
+            bool cupon;
+
+            Console.WriteLine("");
+
+            if (respuesta == 's')
+            {
+                cupon = true;
+                Console.Write("¿Cuál es el porcentaje del descuento? ");
+                double porcentaje = Convert.ToDouble(Console.ReadLine());
+
+                // Imprimiendo el resultado
+                Console.WriteLine($"\nEl cliente {nombre} tiene el correo {email}. El estatus de su cupón es: {cupon}.");
+                Console.WriteLine($"El precio normal del producto es ${precio}.");
+                Console.WriteLine($"El precio después del descuento es ${precio * (1 - porcentaje / 100)}.");
+            } else
+            {
+                cupon = false;
+
+                // Imprimiendo el resultado
+                Console.WriteLine($"El cliente {nombre} tiene el correo {email}. El estatus de su cupón es: {cupon}.");
+                Console.WriteLine($"El precio del producto es ${precio}.");
+                Console.WriteLine("El cliente no cuenta con ningún descuento :(");
+            }
+        }
+    }
+}
---
+++ ejercicio_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lenguajes = new List<string>()
+            {
+                "1.- C#.",
+                "2.- Java.",
+                "3.- Python.",
+                "4.- JavaScript.",
+                "5.- PHP.",
+                "6.- Go."
+            };
+
+            foreach (var lenguaje in lenguajes)
+            {
+                Console.WriteLine(lenguaje);
+            }
+
+            Console.Write("Escriba el número del lenguaje que le interese: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            switch (numero) {
+                case 1:
+                    Console.WriteLine("Hola mundo desde C#!");
+                    break;
+                case 2:
+                    Console.WriteLine("Hola mundo desde Java!");
+                    break;
+                case 3:
+                    Console.WriteLine("Hola mundo desde Python!");
+                    break;
+                case 4:
+                    Console.WriteLine("Hola mundo desde JavaScript!");
+                    break;
+                case 5:
+                    Console.WriteLine("Hola mundo desde PHP!");
+                    break;
+                case 6:
+                    Console.WriteLine("Hola mundo desde Go!");
+                    break;
+                default:
+                    Console.WriteLine("El elemento que seleccionado no existe");
+                    break;
+            }
+        }
+    }
+}
